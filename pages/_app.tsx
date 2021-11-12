cfb6aed75219094ddfc5d14f9f332b1389e0d8f5
Samuel Kraft <samuelkraft@me.com>
2021-11-12 23:16:01 +0100

    Add guestbok

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,28 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/700.css";
+
+// 2. Call `extendTheme` and pass your custom values
+export const theme = extendTheme({
+  fonts: {
+    heading: "Inter",
+    body: "Inter",
+  },
+  styles: {
+    global: {
+      body: {
+        background: "#040505",
+        color: "white",
+      },
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,336 @@
+declare let window: any;
+
 import { Button } from "@chakra-ui/button";
+import { FormControl } from "@chakra-ui/form-control";
+import { TimeIcon } from "@chakra-ui/icons";
 import {
-  FormControl,
-  FormHelperText,
-  FormLabel,
-} from "@chakra-ui/form-control";
-import { Box, Container, Heading, Text } from "@chakra-ui/layout";
+  Box,
+  Container,
+  Flex,
+  Heading,
+  Link,
+  Spacer,
+  Stack,
+  Text,
+  VStack,
+} from "@chakra-ui/layout";
 import { Textarea } from "@chakra-ui/textarea";
+import { ethers } from "ethers";
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import TimeAgo from "javascript-time-ago";
+import ReactTimeAgo from "react-time-ago";
+import en from "javascript-time-ago/locale/en.json";
+import abi from "../utils/GuestbookPortal.json";
+import avatar from "../public/avatar.jpeg";
+
+TimeAgo.addDefaultLocale(en);
 
 const Home: NextPage = () => {
-  const isConnected = true;
+  const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
+  const contractABI = abi.abi;
+  const [allWrites, setAllWrites] = useState() as any;
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [message, setMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  /*
+   * Create a method that gets all writes from your contract
+   */
+  const getAllWrites = async () => {
+    const { ethereum } = window;
+
+    try {
+      if (ethereum && contractAddress) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const guestbookPortalContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+        const writes = await guestbookPortalContract.getAllWrites();
+
+        const writesCleaned = writes.map((write) => {
+          return {
+            address: write.writer,
+            timestamp: new Date(write.timestamp * 1000),
+            message: write.message,
+          };
+        });
+
+        setAllWrites(writesCleaned);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  /**
+   * Listen in for emitter events!
+   */
+  useEffect(() => {
+    let guestbookPortalContract;
+
+    const onNewWrite = (from, timestamp, message) => {
+      console.log("NewWrite", from, timestamp, message);
+      setAllWrites((prevState) => [
+        ...prevState,
+        {
+          address: from,
+          timestamp: new Date(timestamp * 1000),
+          message: message,
+        },
+      ]);
+    };
+
+    if (window.ethereum && contractAddress) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+
+      guestbookPortalContract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+      guestbookPortalContract.on("NewWrite", onNewWrite);
+    }
+
+    return () => {
+      if (guestbookPortalContract) {
+        guestbookPortalContract.off("NewWrite", onNewWrite);
+      }
+    };
+  }, []);
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      /*
+       * Check if we're authorized to access the user's wallet
+       */
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+        getAllWrites();
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  /*
+   * This runs our function when the page loads.
+   */
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  /**
+   * Implement your connectWallet method here
+   */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const write = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum && contractAddress) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const guestbookPortalContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        let count = await guestbookPortalContract.getTotalWrites();
+        console.log("Retrieved total writes count...", count.toNumber());
+
+        /*
+         * Execute the actual wave from your smart contract
+         */
+        const waveTxn = await guestbookPortalContract.write(message, {
+          gasLimit: 300000,
+        });
+        console.log("Mining...", waveTxn.hash);
+        setIsLoading(true);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+        setMessage("");
+
+        count = await guestbookPortalContract.getTotalWrites();
+        console.log("Retrieved total writes count...", count.toNumber());
+        setIsLoading(false);
+      } else {
+        console.log("Ethereum object or contractAddress doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <Container>
+    <Container maxW="container.md">
       <Head>
         <title>Web3 guestbook</title>
         <meta name="description" content="My web3 guestbook" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Box py={6}>
-        <Box mb={6}>
-          <Heading>Welcome to my web3 guestbook</Heading>
-          <Text>
-            Login with your Ethereum wallet to leave a message. This smart
-            contract is deployed on the Rinkeby testnet, so you can test without
-            spending any (real) gas.
+        <VStack mb={6} spacing={4} pt={[20, 32]} pb={8} align="center">
+          <Box borderRadius="full" overflow="hidden" fontSize={0}>
+            <Image
+              src={avatar}
+              width={90}
+              height={90}
+              alt="Samuel Kraft avatar"
+            />
+          </Box>
+          <Heading textAlign="center">
+            Welcome to Samuels web3 guestbook
+          </Heading>
+          <Text align="center" opacity={0.75}>
+            Connect your{" "}
+            <Link
+              href="https://metamask.io/"
+              isExternal
+              textDecoration="underline"
+            >
+              Metamask wallet
+            </Link>{" "}
+            to leave a message. This smart contract is deployed on the Rinkeby
+            testnet, so you can gm me without spending any real gas.
           </Text>
-        </Box>
-        <Box p={3} background="gray.100" borderRadius={12}>
-          {isConnected ? (
+        </VStack>
+        <Box p={6} background="gray.900" borderRadius={12} mb={16}>
+          {currentAccount ? (
             <>
-              <FormControl id="message" mb={3}>
-                <FormLabel>Message</FormLabel>
-                <Textarea background="white" />
+              <Flex mb={4} align="center">
+                <Box
+                  w="10px"
+                  h="10px"
+                  bg="green.300"
+                  borderRadius="full"
+                  mr={2}
+                  flexShrink={0}
+                />
+                <Text
+                  overflow="hidden"
+                  textOverflow="ellipsis"
+                  whiteSpace="nowrap"
+                >
+                  MetaMask Connected: {currentAccount}
+                </Text>
+              </Flex>
+              <FormControl id="message" mb={4} isRequired>
+                <Textarea
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  disabled={isLoading}
+                  placeholder="Message"
+                  background="#040505"
+                  border="gray.600"
+                  _focus={{ outline: "2px solid #DD6B20" }}
+                />
               </FormControl>
-              <Button type="submit" background="green.300" isFullWidth>
-                Submit
+              <Button
+                onClick={write}
+                isFullWidth
+                isLoading={isLoading}
+                loadingText="Mining…"
+                disabled={!message || isLoading}
+                colorScheme="orange"
+              >
+                Post Message
               </Button>
             </>
           ) : (
-            <Button background="blue.300">Connect Wallet</Button>
+            <Button colorScheme="orange" onClick={connectWallet}>
+              Connect Wallet
+            </Button>
+          )}
+        </Box>
+        <Box mb={12}>
+          {allWrites && (
+            <VStack spacing={10}>
+              {allWrites
+                .filter((write) => write.message)
+                .sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp))
+                .map(({ message, timestamp, address }) => {
+                  return (
+                    <Box key={message} w="100%">
+                      <Flex align="center" fontSize={14} opacity={0.75} mb={3}>
+                        <Text
+                          overflow="hidden"
+                          textOverflow="ellipsis"
+                          whiteSpace="nowrap"
+                          maxW={["150px", "280px", "none"]}
+                        >
+                          From: {address}
+                        </Text>
+                        <Box px={2}>·</Box>
+                        <Flex
+                          align="center"
+                          overflow="hidden"
+                          whiteSpace="nowrap"
+                        >
+                          <TimeIcon mr={1.5} />
+                          <ReactTimeAgo
+                            date={new Date(timestamp.toString())}
+                            locale="en-US"
+                          />
+                        </Flex>
+                      </Flex>
+                      <Box
+                        bg="#0b93f6"
+                        p={[4, 5]}
+                        borderRadius={25}
+                        borderTopLeftRadius={1}
+                        display="inline-flex"
+                      >
+                        <Text fontSize={[16, 20]}>{message}</Text>
+                      </Box>
+                    </Box>
+                  );
+                })}
+            </VStack>
           )}
         </Box>
       </Box>
